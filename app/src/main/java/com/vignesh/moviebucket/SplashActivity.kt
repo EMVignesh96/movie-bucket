f8bc0046e14f2e672bc867bc95262c822964556b
Vignesh <emvignesh16@gmail.com>
2020-08-16 16:17:24 +0530

    Add Splash screen

--- app/src/main/java/com/vignesh/moviebucket/SplashActivity.kt
+++ app/src/main/java/com/vignesh/moviebucket/SplashActivity.kt
@@ -16,32 +16,33 @@
 
 package com.vignesh.moviebucket
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.vignesh.moviebucket.databinding.ActivityMainBinding
+import com.vignesh.moviebucket.databinding.ActivitySplashBinding
+import com.vignesh.moviebucket.worker.DownloadLibrariesWorker
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
+
+    override fun onResume() {
+        super.onResume()
+        DownloadLibrariesWorker.startWork(baseContext)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+        val binding: ActivitySplashBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_splash)
         with(binding) {
-            root.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            root.systemUiVisibility = android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         }
-
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-
-        navView.setupWithNavController(navController)
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 1000)
     }
 }
